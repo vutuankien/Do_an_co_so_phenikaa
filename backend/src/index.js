4f60d9a78b94c3e767490ddbf85e51b7f1e986d1
vutuankien <vutuankien030904@gmail.com>
2025-05-21 13:40:51 +0700

    change cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({ limit: '50mb' }));
 app.use(
     cors({
-        origin: 'http://localhost:5173', // Cho phép frontend React
+        origin: ['https://vite-react-ecommerce-fr-git-50e2dd-kien-tuans-projects-984365e8.vercel.app'], // Cho phép frontend React
         methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS', // Thêm PATCH vào danh sách
         allowedHeaders: 'Content-Type,Authorization',
         credentials: true,
